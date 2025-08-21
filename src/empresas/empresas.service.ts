b7c96e71eefe144a02a41971169cbbaa4800a277
Diego Donat <diegodonatr@gmail.com>
2025-08-21 19:58:48 -0400

    cambios menores

--- src/empresas/empresas.service.ts
+++ src/empresas/empresas.service.ts
@@ -1,19 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateEmpresaDto } from './dto/create-empresa.dto';
 import { UpdateEmpresaDto } from './dto/update-empresa.dto';
+import { Empresa } from './entities/empresa.entity';
 
 @Injectable()
 export class EmpresasService {
-  create(createEmpresaDto: CreateEmpresaDto) {
-    return 'This action adds a new empresa';
+
+  empresas: Empresa[] = [];
+
+  create(createEmpresaDto: CreateEmpresaDto): void{
+    if (this.empresas.find(empresa => empresa.nombre.toLowerCase() === createEmpresaDto.nombre.toLowerCase())) {
+      throw new BadRequestException('Ya existe una empresa con ese nombre');
+    }
+
+    const empresaCreada = new Empresa(
+      this.empresas.length + 1,
+      createEmpresaDto.nombre,
+      createEmpresaDto.sitioWeb,
+      createEmpresaDto.tipo
+    )
+    this.empresas.push(empresaCreada);
   }
 
   findAll() {
     return `This action returns all empresas`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} empresa`;
+  findOne(id: number): Empresa | undefined {
+    const empresaEncontrada = this.empresas.find(empresa => empresa.id = +id);
+    if (!empresaEncontrada) {
+      throw new NotFoundException('Empresa no encontrada');
+    }
+    return empresaEncontrada;
   }
 
   update(id: number, updateEmpresaDto: UpdateEmpresaDto) {
